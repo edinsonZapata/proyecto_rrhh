93098de677af4049c03e940dcd6fe7eeae37b696
Edinson Zapata <edinson.zapata@smartsoft.com.co>
2020-10-15 19:24:00 -0500

    Bootstrap

--- src/paginas/home/Home.jsx
+++ src/paginas/home/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./Home.css";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const Home = () => {
     return (
-        <div>
+        <div className="home-titulo">
             <h1>!Hola!</h1>
             <label>Bienvenido de nuevo</label>
         </div>
